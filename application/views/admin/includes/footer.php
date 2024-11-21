28b6e7db555b078c49c1796e4c36627ce6776e0d
Priya Gauatm <gautampriya572000@gmail.com>
2024-11-21 17:22:52 +0530

    Update footer.php

--- application/views/admin/includes/footer.php
+++ application/views/admin/includes/footer.php
@@ -1,7 +1,7 @@
       <footer class="sticky-footer">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <span>Copyright © <a href="https://phpgurukul.com" target="_blank">PHGurukul</a> 2018</span>
+              <span>Copyright © Priya Gautam</span>
             </div>
           </div>
         </footer>
